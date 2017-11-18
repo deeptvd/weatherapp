8f4915c1143acca4815ef3c6be0c398fcdf8c36e
Deep Trivedi <deeptvd@gmail.com>
2017-11-18 16:14:23 -0500

    Add POST /api/weather

--- app.js
+++ app.js
@@ -14,6 +14,23 @@ app.get('/', (req, res) => {
   res.send("INDEX");
 });
 
+//Weather Route
+app.post('/api/weather', (req, res) => {
+  const zip = req.body.zipcode;
+  const appSecret = "0f6f539a51844dc80364edb30889276b";
+  const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${appSecret}`;
+
+  axios.get(weatherUrl).then((response) => {
+    res.status(200).send(response.data);
+  }).catch((e) => {
+    if(e.code === 'ENOTFOUND'){
+      console.log('Unable to connect to API servers');
+    }else {
+      console.log(e.message);
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
