db7c6e85a35c1f5e58f58c5fb5e2a40a7b102aee
Deep Trivedi <deeptvd@gmail.com>
2017-11-18 23:07:47 -0500

    Completed all views

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session'); 
 const axios = require('axios');
 
 const app = express();
@@ -8,25 +10,58 @@ const app = express();
 const port = 3000;
 
 //Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//Express Session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Flash Middleware
+app.use(flash());
+
+//Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next(); 
+});
+
 //Index Route
 app.get('/', (req, res) => {
-  res.send("INDEX");
+  const title = "Welcome";
+  res.render('index', {
+    title:title
+  });
+});
+
+//About Route
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+//Weather Page
+app.get('/api/weather', (req, res) => {
+  res.render('weather');
 });
 
 //Weather Route
-app.get('/api/weather/:zip', (req, res) => {
-  const zip = req.params.zip;
+app.post('/api/weather', (req, res) => {
+  const zip = req.body.zipcode;
   const units = "imperial";
   const appSecret = "0f6f539a51844dc80364edb30889276b";
   const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${appSecret}&units=${units}`;
 
-  axios.get(weatherUrl).then((response) => {
+  axios.get(weatherUrl)
+    .then((response) => {
     const result = {
       description: response.data.weather[0].description,
       temp: response.data.main.temp,
@@ -34,12 +69,15 @@ app.get('/api/weather/:zip', (req, res) => {
       windSpeed: response.data.wind.speed,
       cityName: response.data.name,
     };
-    res.status(200).send(result);
+    res.render('weather', {
+      result: result
+    });
   }).catch((e) => {
     if(e.code === 'ENOTFOUND'){
       console.log('Unable to connect to API servers');
     }else {
-      console.log(e.message);
+      req.flash('error_msg', 'Please enter valid zipcode');
+      res.redirect('/api/weather')
     }
   });
 });
